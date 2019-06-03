876b155ab9ac96d88ae45f315314fe4fa104e5eb
ncritiansa <ncristiansalinasandia@gmail.com>
2019-06-03 18:14:25 +0200

    Terminado

---
+++ mysite/polls/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+	url('', views.IndexView.as_view(), name='index'),
+    url('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    url('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    url('<int:question_id>/vote/', views.vote, name='vote'),
+]
